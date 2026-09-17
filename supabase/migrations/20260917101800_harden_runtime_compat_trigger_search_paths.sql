530b2972526d0544fc9435b957309ff0531d2f89
Timileyin Ayomide Salawudeen <timzeeworld@gmail.com>
2026-09-17 11:18:43 +0100

    Harden compatibility trigger search paths

---
+++ supabase/migrations/20260917101800_harden_runtime_compat_trigger_search_paths.sql
@@ -0,0 +1,4 @@
+ALTER FUNCTION public.sync_notifications_compat_columns() SET search_path = pg_catalog, public;
+ALTER FUNCTION public.sync_ai_prompts_compat_columns() SET search_path = pg_catalog, public;
+ALTER FUNCTION public.sync_curator_sources_compat_columns() SET search_path = pg_catalog, public;
+ALTER FUNCTION public.sync_curator_posts_compat_columns() SET search_path = pg_catalog, public;
